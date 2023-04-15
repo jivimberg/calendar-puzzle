5a27b9332297764f2b7cbc81b16896398d91c064
Juan Vimberg <jvimberg@netflix.com>
2023-04-15 16:59:28 -0700

    Created empty board with hardcoded date

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+mod board;
+
+use board::Board;
+use time::macros::date;
+
 fn main() {
-    println!("Hello, world!");
+    let board = Board::new(date!(2023 - 02 - 12));
+    println!("{}", board);
 }
