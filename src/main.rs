806c01ebdfe5d3a492d39d3a8cff4a5537ad58ac
Juan Vimberg <jvimberg@netflix.com>
2023-04-18 01:25:29 -0700

    Coded all pieces

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,10 @@ fn main() {
     let board = Board::new(date!(2023 - 02 - 12));
     println!("{}", board);
 
-    // testing transpose
-    let m = vec![
-        vec!["🟦", "🟦", "🟦"],
-        vec!["  ", "🟦", "  "],
-        vec!["  ", "🟦", "  "]
-    ];
-    let shape = Shape { tile_matrix: m };
-
-    let distinct_shapes = shape.generate_all_distinct_variations();
-    for shape in distinct_shapes {
-        println!("{}", shape);
+    for piece in piece::Piece::get_all_pieces().iter() {
+        println!("{}", piece.name);
+        for shape in piece.distinct_shapes.iter() {
+            println!("{}", shape);
+        }
     }
 }
--- src/piece.rs
+++ src/piece.rs
@@ -1,15 +1,84 @@
+use std::collections::HashSet;
 use crate::shape::Shape;
 
-struct Piece {
-    name: String,
-    distinct_shapes: Vec<Shape>
+pub(crate) struct Piece {
+    pub(crate) name: String,
+    pub(crate) distinct_shapes: HashSet<Shape>
 }
 
 impl Piece {
     fn new(name: String, base_shape: Shape) -> Self {
         Self {
             name,
-            distinct_shapes: vec![], // TODO
+            distinct_shapes: base_shape.generate_all_distinct_variations(),
         }
+    }
+
+    pub fn get_all_pieces() -> Vec<Piece> {
+        vec![
+            Piece::new("I".to_string(), Shape {
+                tile_matrix: vec![
+                    vec!["🟦", "🟦", "🟦", "🟦"],
+                ]
+            }),
+            Piece::new("Short L".to_string(), Shape {
+                tile_matrix: vec![
+                    vec!["🟦", "🟦", "🟦"],
+                    vec!["  ", "  ", "🟦"],
+                ]
+            }),
+            Piece::new("Long L".to_string(), Shape {
+                tile_matrix: vec![
+                    vec!["🟦", "🟦", "🟦", "🟦"],
+                    vec!["  ", "  ", "  ", "🟦"],
+                ]
+            }),
+            Piece::new("T".to_string(), Shape {
+                tile_matrix: vec![
+                    vec!["🟦", "🟦", "🟦"],
+                    vec!["  ", "🟦", "  "],
+                    vec!["  ", "🟦", "  "],
+                ]
+            }),
+            Piece::new("S".to_string(), Shape {
+                tile_matrix: vec![
+                    vec!["  ", "🟦", "🟦"],
+                    vec!["🟦", "🟦", "  "],
+                ]
+            }),
+            Piece::new(".S".to_string(), Shape {
+                tile_matrix: vec![
+                    vec!["  ", "  ", "🟦", "🟦"],
+                    vec!["🟦", "🟦", "🟦", "  "],
+                ]
+            }),
+            Piece::new("d".to_string(), Shape {
+                tile_matrix: vec![
+                    vec!["  ", "🟦"],
+                    vec!["🟦", "🟦"],
+                    vec!["🟦", "🟦"],
+                ]
+            }),
+            Piece::new("U".to_string(), Shape {
+                tile_matrix: vec![
+                    vec!["🟦", "  ", "🟦"],
+                    vec!["🟦", "🟦", "🟦"],
+                ]
+            }),
+            Piece::new("Corner".to_string(), Shape {
+                tile_matrix: vec![
+                    vec!["🟦", "🟦", "🟦"],
+                    vec!["🟦", "  ", "  "],
+                    vec!["🟦", "  ", "  "],
+                ]
+            }),
+            Piece::new("Z".to_string(), Shape {
+                tile_matrix: vec![
+                    vec!["🟦", "🟦", "  "],
+                    vec!["  ", "🟦", "  "],
+                    vec!["  ", "🟦", "🟦"],
+                ]
+            }),
+        ]
     }
 }
